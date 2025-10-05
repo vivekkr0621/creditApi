d342f37fe27016d19c47a2c3d265211d36c2e53c
vivek <vivekkr0621@gmail.com>
2025-10-05 22:27:56 +0530

    remove ui to seperate repo

--- investment-plan/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    skipLogin: false,                 // login required in production
-    apiUrl: 'https://your-production-api.com/api' // production backend URL
-};
--- investment-plan/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    skipLogin: true,                  // dev-only flag to skip login
-    apiUrl: 'https://localhost:7185/api'  // backend URL for development
-};
